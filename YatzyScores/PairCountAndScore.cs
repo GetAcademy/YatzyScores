b3adc41ba1a3b5d373413031b81a5f3aebdec305
GET Academy <terje@kolderup.net>
2021-02-16 12:53:25 +0100

    Initial commit

---
+++ YatzyScores/PairCountAndScore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YatzyScores
+{
+    class PairCountAndScore
+    {
+        public int PairCount;
+        public int Score;
+
+        public int TwoPairScore
+        {
+            get
+            {
+                var hasTwoPairs = PairCount >= 2;
+                return hasTwoPairs ? Score : 0;
+            }
+        }
+
+        public void Add(int diceValue, int frequency)
+        {
+            var hasTwoOrMore = frequency > 1;
+            if (!hasTwoOrMore) return;
+            PairCount++;
+            Score += diceValue * 2;
+        }
+    }
+}
--- YatzyScores/Yatzy.cs
+++ YatzyScores/Yatzy.cs
@@ -8,35 +8,63 @@ namespace YatzyScores
     {
         private int[] _values;
 
-        public Yatzy()
-        {
-
-        }
-
         public Yatzy(params int[] values)
         {
             _values = values;
         }
 
+        /*
+         * 1: Introdusere variabel
+         * 2: Introdusere metode
+         * 3: Introdusere klasse
+
+            public int GetTwoPairPoints()
+            {
+                var frequencies = new int[7];
+                foreach (var value in _values)
+                {
+                    frequencies[value]++;
+                }
+                var points = 0;
+                var score = 0;
+                for (var diceValue = 6; diceValue > 0; diceValue--)
+                {
+                    var hasTwoOrMore = frequencies[diceValue] > 1;
+                    if (hasTwoOrMore)
+                    {
+                        points++;
+                        score += diceValue * 2;
+                    }
+                }
+                return points >= 2 ? score : 0;
+            }         
+         */
+
         public int GetTwoPairPoints()
         {
-            var f = new int[7];
-            foreach (var v in _values)
+            var frequencies = CreateFrequencies(_values);
+            var score = CalcPairCountAndScore(frequencies);
+            return score.TwoPairScore;
+        }
+
+        private static PairCountAndScore CalcPairCountAndScore(int[] frequencies)
+        {
+            var score = new PairCountAndScore();
+            for (var diceValue = 6; diceValue > 0; diceValue--)
             {
-                f[v]++;
+                score.Add(diceValue, frequencies[diceValue]);
             }
-            var p = 0;
-            var s = 0;
-            for (var v = 6; v > 0; v--)
+            return score;
+        }
+
+        private static int[] CreateFrequencies(int[] values)
+        {
+            var frequencies = new int[7];
+            foreach (var value in values)
             {
-                if (f[v] > 1)
-                {
-                    p++;
-                    s += v * 2;
-                }
+                frequencies[value]++;
             }
-            return p >= 2 ? s : 0;
+            return frequencies;
         }
     }
-
 }
